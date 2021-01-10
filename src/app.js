da6eea0bc7f1c26d22d0356953da1d3771a57a4a
Jason Zhang <zhang98@bu.edu>
2021-01-10 16:15:50 -0500

    Added react-router for multiple pages and a 404 error page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,50 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Hello World</p>, document.getElementById('app'));
+const dashBoard = () => (
+    <div>
+        This is the dashboard.
+    </div>
+)
+
+const addExpensePage = () => (
+    <div>
+        This is the page to add an expense.
+    </div>
+)
+
+const editExpensePage = () => (
+    <div>
+        This page is to edit the expenses.
+    </div>
+)
+
+const helpPage = () => (
+    <div>
+        This is the help page.
+    </div>
+)
+
+const notFoundPage = () => (
+    <div>
+        404 Error!
+    </div>
+)
+
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={dashBoard} exact={true}/>
+            <Route path="/create" component={addExpensePage} />
+            <Route path="/editExpense" component={editExpensePage} />
+            <Route path="/help" component={helpPage} />
+            <Route component={notFoundPage} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
